202fcf8c3d0f0c9ceb3be99f5b35001cc9e2cc48
Tianorder <tianfanzhi@qq.com>
2023-06-29 19:09:17 +0800

    Create don't-panic-episode-1.py

---
+++ puzzles/medium/don't-panic-episode-1/don't-panic-episode-1.py
@@ -0,0 +1,31 @@
+# the standard input according to the problem statement.
+
+# nb_floors: number of floors
+# width: width of the area
+# nb_rounds: maximum number of rounds
+# exit_floor: floor on which the exit is found
+# exit_pos: position of the exit on its floor
+# nb_total_clones: number of generated clones
+# nb_additional_elevators: ignore (always zero)
+# nb_elevators: number of elevators
+nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in input().split()]
+dic = {}
+dic[exit_floor] = exit_pos
+for i in range(nb_elevators):
+    # elevator_floor: floor on which this elevator is found
+    # elevator_pos: position of the elevator on its floor
+    elevator_floor, elevator_pos = [int(j) for j in input().split()]
+    dic[elevator_floor] = elevator_pos
+
+# game loop
+while True:
+    inputs = input().split()
+    clone_floor = int(inputs[0])  # floor of the leading clone
+    clone_pos = int(inputs[1])  # position of the leading clone on its floor
+    direction = inputs[2]  # direction of the leading clone: LEFT or RIGHT
+
+    if clone_floor == -1 or (dic[clone_floor] >= clone_pos and direction == 'RIGHT') or (dic[clone_floor] <= clone_pos and direction == 'LEFT'):
+        # action: WAIT or BLOCK
+        print("WAIT")
+    else:
+        print("BLOCK")
