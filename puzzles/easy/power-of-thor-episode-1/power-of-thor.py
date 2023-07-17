60fc32571db8678a668d1b50ea640af59bf4c398
Tianorder <tianfanzhi@qq.com>
2023-07-17 14:43:44 +0800

    歪门邪道，但考验基本功

--- puzzles/easy/power-of-thor-episode-1/power-of-thor.py
+++ puzzles/easy/power-of-thor-episode-1/power-of-thor.py
@@ -1,17 +1,13 @@
 a,b,x,y=map(int,input().split())
-while True:
- input()
- o,p="",""
+while 1:
+ o=p=""
  if b>y:
-  o="S"
-  y+=1
+  o,y="S",y+1
  elif b<y:
-  o="N"
-  y-=1
+  o,y="N",y-1
  if a>x:
-  p="E"
-  x+=1
+  p,x="E",x+1
  elif a<x:
-  p="W"
-  x-=1
+  p,x="W",x-1
  print(o+p)
+ 
